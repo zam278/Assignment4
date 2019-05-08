8fda4d3f4255ac406960c94e02ca75e98f968851
Zee <zam278@nyu.edu>
2019-05-08 17:09:45 -0400

    data update

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,97 @@ var map = new mapboxgl.Map({
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
+// a helper function for looking up colors and descriptions for NYC land use codes
+var LandUseLookup = (code) => {
+  switch (code) {
+    case 1:
+      return {
+        color: '#f4f455',
+        description: '1 & 2 Family',
+      };
+    case 2:
+      return {
+        color: '#f7d496',
+        description: 'Multifamily Walk-up',
+      };
+    case 3:
+      return {
+        color: '#FF9900',
+        description: 'Multifamily Elevator',
+      };
+    case 4:
+      return {
+        color: '#f7cabf',
+        description: 'Mixed Res. & Commercial',
+      };
+    case 5:
+      return {
+        color: '#ea6661',
+        description: 'Commercial & Office',
+      };
+    case 6:
+      return {
+        color: '#d36ff4',
+        description: 'Industrial & Manufacturing',
+      };
+    case 7:
+      return {
+        color: '#dac0e8',
+        description: 'Transportation & Utility',
+      };
+    case 8:
+      return {
+        color: '#5CA2D1',
+        description: 'Public Facilities & Institutions',
+      };
+    case 9:
+      return {
+        color: '#8ece7c',
+        description: 'Open Space & Outdoor Recreation',
+      };
+    case 10:
+      return {
+        color: '#bab8b6',
+        description: 'Parking Facilities',
+      };
+    case 11:
+      return {
+        color: '#5f5f60',
+        description: 'Vacant Land',
+      };
+    case 12:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+    default:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+  }
+};
+
+// use jquery to programmatically create a Legend
+// for numbers 1 - 11, get the land use color and description
+for (var i=1; i<12; i++) {
+  // lookup the landuse info for the current iteration
+  const landuseInfo = LandUseLookup(i);
+
+  // this is a simple jQuery template, it will append a div to the legend with the color and description
+  $('.legend').append(`
+    <div>
+      <div class="legend-color-box" style="background-color:${landuseInfo.color};"></div>
+      ${landuseInfo.description}
+    </div>
+  `)
+}
+
+// a little object for looking up neighborhood center points
+var neighborHoodLookup = {
+  'china-town': [-74.003660, 40.713034],
+  'lenox-Hill': [-73.9732269, 40.7682908],
+  'upper-east-side': [-73.9666769, 40.7682912],
+  'east-harlem': [-73.938843, 40.805212],
+}
