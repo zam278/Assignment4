4e5075dae341309644c0625b897045dac745199d
Zee <zam278@nyu.edu>
2019-05-08 18:34:26 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-
 // sets up my mapbox access token so they can track my usage of their basemap services
-mapboxgl.accessToken = 'pk.eyJ1IjoiemFtMjc4IiwiYSI6ImNqdWQ5YWpneDBzbGQ0ZW5xOGtzcDJ0OGcifQ.PqU5et-zFUgXyhIeGuueqg',
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
 
-// instantiate the map
+// creaate the map
 var map = new mapboxgl.Map({
   container: 'mapContainer',
   style: 'mapbox://styles/mapbox/light-v9',
   center: [-73.98, 40.70],
   zoom: 9.5,
 });
+
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
