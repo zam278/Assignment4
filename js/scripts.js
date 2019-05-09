70f6ebb0770cb3009e8a1ad77f5479458f9d5dc9
Zee <zam278@nyu.edu>
2019-05-08 23:10:44 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -263,144 +263,162 @@ map.on('style.load', function() {
    }
  })
 
-///////////////////////////////////////////////////////////////////
- // this sets up the geojson as a source in the map, which I can use to add visual layers
- map.addSource('east-harlem', {
-   type: 'geojson',
-   data: './data/east-harlem.geojson',
- });
-
- // add a custom-styled layer for tax lots
-map.addLayer({
-  id: 'east-harlem-lots-fill',
-  type: 'fill',
-  source: 'east-harlem',
-  paint: {
-    'fill-opacity': 0.7,
-    'fill-color': {
-      type: 'categorical',
-      property: 'landuse',
-      stops: [
-          [
-            '01',
-            LandUseLookup(1).color,
-          ],
-          [
-            "02",
-            LandUseLookup(2).color,
-          ],
-          [
-            "03",
-            LandUseLookup(3).color,
-          ],
-          [
-            "04",
-            LandUseLookup(4).color,
-          ],
-          [
-            "05",
-            LandUseLookup(5).color,
-          ],
-          [
-            "06",
-            LandUseLookup(6).color,
-          ],
-          [
-            "07",
-            LandUseLookup(7).color,
-          ],
-          [
-            "08",
-            LandUseLookup(8).color,
-          ],
-          [
-            "09",
-            LandUseLookup(9).color,
-          ],
-          [
-            "10",
-            LandUseLookup(10).color,
-          ],
-          [
-            "11",
-            LandUseLookup(11).color,
-          ],
-        ]
-      }
-  }
-}, 'waterway-label')
-
-// add an outline to the tax lots which is only visible after zoom level 14.8
-map.addLayer({
-  id: 'east-harlem-lots-line',
-  type: 'line',
-  source: 'east-harlem',
-  paint: {
-    'line-opacity': 0.7,
-    'line-color': 'gray',
-    'line-opacity': {
-      stops: [[14, 0], [14.8, 1]], // zoom-dependent opacity, the lines will fade in between zoom level 14 and 14.8
-    }
-  }
-});
-
-
-// add an empty data source, which we will use to highlight the lot the user is hovering over
-map.addSource('highlight-feature', {
-  type: 'geojson',
-  data: {
-    type: 'FeatureCollection',
-    features: []
-  }
 })
+///////////////////////////////////////////////////////////////////////////////////////////////
+// we can't add our own sources and layers until the base style is finished loading
+map.on('style.load', function() {
+  // add a button click listener that will control the map
+  // we have 4 buttons, but can listen for clicks on any of them with just one listener
+  $('.flyto').on('click', function(e) {
+    // pull out the data attribute for the neighborhood using query
+    var neighborhood = $(e.target).data('neighborhood');
 
-// add a layer for the highlighted lot
-map.addLayer({
-  id: 'highlight-line',
-  type: 'line',
-  source: 'highlight-feature',
-  paint: {
-    'line-width': 3,
-    'line-opacity': 0.9,
-    'line-color': 'black',
-  }
-});
+    // this is a useful notation for looking up a key in an object using a variable
+    var center = neighborHoodLookup[neighborhood];
 
-// when the mouse moves, do stuff!
-map.on('mousemove', function (e) {
-  // query for the features under the mouse, but only in the lots layer
-  var features = map.queryRenderedFeatures(e.point, {
-      layers: ['east-harlem-lots-fill'],
+    // fly to the neighborhood's center point
+    map.flyTo({center: center, zoom: 14});
   });
 
-  // get the first feature from the array of returned features.
-  var lot = features[0]
+  // let's hack the basemap style a bit
+  // you can use map.getStyle() in the console to inspect the basemap layers
+  map.setPaintProperty('water', 'fill-color', '#a4bee8')
 
-  if (lot) {  // if there's a lot under the mouse, do stuff
-    map.getCanvas().style.cursor = 'pointer';
-// make the cursor a pointer
+  // this sets up the geojson as a source in the map, which I can use to add visual layers
+  map.addSource('east-harlem', {
+    type: 'geojson',
+    data: './data/east-harlem.geojson',
+  });
 
-    // lookup the corresponding description for the land use code
-    var landuseDescription = LandUseLookup(parseInt(lot.properties.landuse)).description;
+  // add a custom-styled layer for tax lots
+ map.addLayer({
+   id: 'ceast-harlem-lots-fill',
+   type: 'fill',
+   source: 'east-harlem',
+   paint: {
+     'fill-opacity': 0.7,
+     'fill-color': {
+       type: 'categorical',
+       property: 'landuse',
+       stops: [
+           [
+             '01',
+             LandUseLookup(1).color,
+           ],
+           [
+             "02",
+             LandUseLookup(2).color,
+           ],
+           [
+             "03",
+             LandUseLookup(3).color,
+           ],
+           [
+             "04",
+             LandUseLookup(4).color,
+           ],
+           [
+             "05",
+             LandUseLookup(5).color,
+           ],
+           [
+             "06",
+             LandUseLookup(6).color,
+           ],
+           [
+             "07",
+             LandUseLookup(7).color,
+           ],
+           [
+             "08",
+             LandUseLookup(8).color,
+           ],
+           [
+             "09",
+             LandUseLookup(9).color,
+           ],
+           [
+             "10",
+             LandUseLookup(10).color,
+           ],
+           [
+             "11",
+             LandUseLookup(11).color,
+           ],
+         ]
+       }
+   }
+ }, 'waterway-label')
 
-    // use jquery to display the address and land use description to the sidebar
-    $('#address').text(lot.properties.address);
-    $('#bldgclass').text(lot.properties.bldgclass);
-    $('#landuse').text(landuseDescription);
-
-    // set this lot's polygon feature as the data for the highlight source
-    map.getSource('highlight-feature').setData(lot.geometry);
-  } else {
-    map.getCanvas().style.cursor = 'default'; // make the cursor default
-
-    // reset the highlight source to an empty featurecollection
-    map.getSource('highlight-feature').setData({
-      type: 'FeatureCollection',
-      features: []
-    });
-  }
-})
+ // add an outline to the tax lots which is only visible after zoom level 14.8
+ map.addLayer({
+   id: 'east-harlem-lots-line',
+   type: 'line',
+   source: 'east-harlem',
+   paint: {
+     'line-opacity': 0.7,
+     'line-color': 'gray',
+     'line-opacity': {
+       stops: [[14, 0], [14.8, 1]], // zoom-dependent opacity, the lines will fade in between zoom level 14 and 14.8
+     }
+   }
+ });
 
 
+ // add an empty data source, which we will use to highlight the lot the user is hovering over
+ map.addSource('highlight-feature', {
+   type: 'geojson',
+   data: {
+     type: 'FeatureCollection',
+     features: []
+   }
+ })
+
+ // add a layer for the highlighted lot
+ map.addLayer({
+   id: 'highlight-line',
+   type: 'line',
+   source: 'highlight-feature',
+   paint: {
+     'line-width': 3,
+     'line-opacity': 0.9,
+     'line-color': 'black',
+   }
+ });
+
+ // when the mouse moves, do stuff!
+ map.on('mousemove', function (e) {
+   // query for the features under the mouse, but only in the lots layer
+   var features = map.queryRenderedFeatures(e.point, {
+       layers: ['east-harlem-lots-fill'],
+   });
+
+   // get the first feature from the array of returned features.
+   var lot = features[0]
+
+   if (lot) {  // if there's a lot under the mouse, do stuff
+     map.getCanvas().style.cursor = 'pointer';
+ // make the cursor a pointer
+
+     // lookup the corresponding description for the land use code
+     var landuseDescription = LandUseLookup(parseInt(lot.properties.landuse)).description;
+
+     // use jquery to display the address and land use description to the sidebar
+     $('#address').text(lot.properties.address);
+     $('#bldgclass').text(lot.properties.bldgclass);
+     $('#landuse').text(landuseDescription);
+
+     // set this lot's polygon feature as the data for the highlight source
+     map.getSource('highlight-feature').setData(lot.geometry);
+   } else {
+     map.getCanvas().style.cursor = 'default'; // make the cursor default
+
+     // reset the highlight source to an empty featurecollection
+     map.getSource('highlight-feature').setData({
+       type: 'FeatureCollection',
+       features: []
+     });
+   }
+ })
 
 })
