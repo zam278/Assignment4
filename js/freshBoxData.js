71d2e945f2555e8b7d45389d8145309fec6246a1
Zee <zam278@nyu.edu>
2019-05-09 20:01:51 -0400

    final edits

---
+++ js/freshBoxData.js
@@ -0,0 +1,50 @@
+var freshBox =[
+ {
+   "site_name": "Family Health Center of Harlem",
+   "day": "Thursday",
+   "hours": "3:30pm-6:30pm",
+   "coordinator": "Greg Scott",
+   "lng": -73.9459587,
+   "lat": 40.8018631
+ },
+ {
+   "site_name": "Uptown Grand Central",
+   "day": "Wednesday",
+   "hours": "3pm-7pm",
+   "coordinator": "Carey King",
+   "lng": -73.941506,
+   "lat": 40.8036158
+ },
+ {
+   "site_name": "East Harlem Health Action Center",
+   "day": "Thursday",
+   "hours": "3:30pm-6:30pm",
+   "coordinator": "Karen Rojas",
+   "lng": -73.944093,
+   "lat": 40.797606
+ },
+ {
+   "site_name": "Lenox Hill Senior Center",
+   "day": "Tuesday",
+   "hours": "3:30pm-6:30pm",
+   "coordinator": "Idalia Bamert",
+   "lng": -73.9571182,
+   "lat": 40.7667544
+ },
+ {
+   "site_name": "Hunter College",
+   "day": "Thursday",
+   "hours": "3:30pm-6:30pm",
+   "coordinator": "Teresa Johnson",
+   "lng": -73.9666876,
+   "lat": 40.7682952
+ },
+ {
+   "site_name": "1 Centre St",
+   "day": "Thursday",
+   "hours": "2pm-6pm",
+   "coordinator": "Elena Tinschert",
+   "lng": -74.0059089,
+   "lat": 40.7128346
+ }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,77 @@ var map = new mapboxgl.Map({
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+//<=========!!============>//
+// Chinca town Location, 1st center
+// var popup1 = new mapboxgl.Popup({ offset: 40 })
+// //HTML  .setText('Iconic Film Location!');
+//
+// new mapboxgl.Marker()
+//   .setLngLat([-74.0037028, 40.7129173])
+//   .setPopup(popup1)
+//   .addTo(map);
+//   //<=========!!============>//
+// // Lenox Hill location1
+// var popup2 = new mapboxgl.Popup({ offset: 40 })
+// //  .setText('Iconic Film Location!');
+//
+// new mapboxgl.Marker()
+//   .setLngLat([-74.0059089, 40.7128346])
+//   .setPopup(popup2)
+//   .addTo(map);
+//   //<=========!!============>//
+// //Lenox Hill Location 2
+//   var popup3 = new mapboxgl.Popup({ offset: 40 })
+//   //  .setText('Iconic Film Location!');
+//
+//   new mapboxgl.Marker()
+//     .setLngLat([-73.9666876,40.7682952])
+//     .setPopup(popup3)
+//     .addTo(map);
+//     //<=========!!============>//
+// // East Harlem Action Center Location
+//   var popup4 = new mapboxgl.Popup({ offset: 40 })
+//     //  .setText('Iconic Film Location!');
+//
+//     new mapboxgl.Marker()
+//       .setLngLat([-73.944093, 40.797606])
+//       .setPopup(popup4)
+//       .addTo(map);
+//       //<=========!!============>//
+// // FAmily Health Center Location
+//     var popup5 = new mapboxgl.Popup({ offset: 40 })
+//       //  .setText('Iconic Film Location!');
+//
+//       new mapboxgl.Marker()
+//         .setLngLat([-73.9459587, 40.8018631])
+//         .setPopup(popup5)
+//         .addTo(map);
+// //<=========!!============>//
+// //Uptown Grand Central Location
+//     var popup6 = new mapboxgl.Popup({ offset: 40 })
+//       //  .setText('Iconic Film Location!');
+//
+//       new mapboxgl.Marker()
+//         .setLngLat([-73.9413772, 40.8051344])
+//         .setPopup(popup6)
+//         .addTo(map);
+// //<=========!!============>//
+
+//to create a loop for the marker; every for loop it has to start and end with {}
+freshBox.forEach(function(freshBoxData) {
+// to add a marker for each feature
+  new mapboxgl.Marker({
+    color: 'blue',
+  })
+    .setLngLat([freshBoxData.lng, freshBoxData.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 40 })
+      .setHTML('<h4>' + freshBoxData.site_name +'</h4><p>' + "Day: " + freshBoxData.day + '</p><p>'+ "Operating hours: " +
+        freshBoxData.hours + '</p><p>' + "Coordinator: " +
+        freshBoxData.coordinator + '</p>' ))
+    .addTo(map);
+
+
+})
 
 // a helper function for looking up colors and descriptions for NYC land use codes
 var LandUseLookup = (code) => {
@@ -84,11 +155,79 @@ var LandUseLookup = (code) => {
   }
 };
 
+// mapboxgl paint properties for NYC land use codes
+var landUsePaint = {
+  'fill-opacity': 0.7,
+  'fill-color': {
+    type: 'categorical',
+    property: 'landuse',
+    stops: [
+        [
+          '01',
+          LandUseLookup(1).color,
+        ],
+        [
+          "02",
+          LandUseLookup(2).color,
+        ],
+        [
+          "03",
+          LandUseLookup(3).color,
+        ],
+        [
+          "04",
+          LandUseLookup(4).color,
+        ],
+        [
+          "05",
+          LandUseLookup(5).color,
+        ],
+        [
+          "06",
+          LandUseLookup(6).color,
+        ],
+        [
+          "07",
+          LandUseLookup(7).color,
+        ],
+        [
+          "08",
+          LandUseLookup(8).color,
+        ],
+        [
+          "09",
+          LandUseLookup(9).color,
+        ],
+        [
+          "10",
+          LandUseLookup(10).color,
+        ],
+        [
+          "11",
+          LandUseLookup(11).color,
+        ],
+      ]
+    }
+}
+
 // use jquery to programmatically create a Legend
 // for numbers 1 - 11, get the land use color and description
 for (var i=1; i<12; i++) {
   // lookup the landuse info for the current iteration
   const landuseInfo = LandUseLookup(i);
+
+//   new mapboxgl.Marker({
+//   color: 'green',
+// })
+//   .setLngLat([filmData.lon, filmData.lat])
+//   .setPopup(new mapboxgl.Popup({ offset: 40 })
+//     .setHTML('<h4>' + filmData.description +'</h4><p>' + "Year: " + filmData.year + '</p><p>'+ "Rating: " +
+//       filmData.rate + " out of 10" + '</p><p>' + "location: " +
+//       filmData.location + '</p>' ))
+//   .addTo(map);
+
+
+
 
   // this is a simple jQuery template, it will append a div to the legend with the color and description
   $('.legend').append(`
@@ -137,59 +276,7 @@ map.on('style.load', function() {
    id: 'china-town-lots-fill',
    type: 'fill',
    source: 'china-town',
-   paint: {
-     'fill-opacity': 0.7,
-     'fill-color': {
-       type: 'categorical',
-       property: 'landuse',
-       stops: [
-           [
-             '01',
-             LandUseLookup(1).color,
-           ],
-           [
-             "02",
-             LandUseLookup(2).color,
-           ],
-           [
-             "03",
-             LandUseLookup(3).color,
-           ],
-           [
-             "04",
-             LandUseLookup(4).color,
-           ],
-           [
-             "05",
-             LandUseLookup(5).color,
-           ],
-           [
-             "06",
-             LandUseLookup(6).color,
-           ],
-           [
-             "07",
-             LandUseLookup(7).color,
-           ],
-           [
-             "08",
-             LandUseLookup(8).color,
-           ],
-           [
-             "09",
-             LandUseLookup(9).color,
-           ],
-           [
-             "10",
-             LandUseLookup(10).color,
-           ],
-           [
-             "11",
-             LandUseLookup(11).color,
-           ],
-         ]
-       }
-   }
+   paint: landUsePaint
  }, 'waterway-label')
 
  // add an outline to the tax lots which is only visible after zoom level 14.8
@@ -232,11 +319,13 @@ map.on('style.load', function() {
  map.on('mousemove', function (e) {
    // query for the features under the mouse, but only in the lots layer
    var features = map.queryRenderedFeatures(e.point, {
-       layers: ['china-town-lots-fill'],
+       layers: ['china-town-lots-fill', 'east-harlem-lots-fill', 'upper-east-side-lots-fill','lenox-Hill-lots-fill'],
    });
 
    // get the first feature from the array of returned features.
    var lot = features[0]
+
+   console.log(lot);
 
    if (lot) {  // if there's a lot under the mouse, do stuff
      map.getCanvas().style.cursor = 'pointer';
@@ -247,7 +336,7 @@ map.on('style.load', function() {
 
      // use jquery to display the address and land use description to the sidebar
      $('#address').text(lot.properties.address);
-     $('#bldgclass').text(lot.properties.bldgclass);
+     $('#yearbuilt').text(lot.properties.yearbuilt);
      $('#landuse').text(landuseDescription);
 
      // set this lot's polygon feature as the data for the highlight source
@@ -292,62 +381,10 @@ map.on('style.load', function() {
 
   // add a custom-styled layer for tax lots
  map.addLayer({
-   id: 'ceast-harlem-lots-fill',
+   id: 'east-harlem-lots-fill',
    type: 'fill',
    source: 'east-harlem',
-   paint: {
-     'fill-opacity': 0.7,
-     'fill-color': {
-       type: 'categorical',
-       property: 'landuse',
-       stops: [
-           [
-             '01',
-             LandUseLookup(1).color,
-           ],
-           [
-             "02",
-             LandUseLookup(2).color,
-           ],
-           [
-             "03",
-             LandUseLookup(3).color,
-           ],
-           [
-             "04",
-             LandUseLookup(4).color,
-           ],
-           [
-             "05",
-             LandUseLookup(5).color,
-           ],
-           [
-             "06",
-             LandUseLookup(6).color,
-           ],
-           [
-             "07",
-             LandUseLookup(7).color,
-           ],
-           [
-             "08",
-             LandUseLookup(8).color,
-           ],
-           [
-             "09",
-             LandUseLookup(9).color,
-           ],
-           [
-             "10",
-             LandUseLookup(10).color,
-           ],
-           [
-             "11",
-             LandUseLookup(11).color,
-           ],
-         ]
-       }
-   }
+   paint: landUsePaint,
  }, 'waterway-label')
 
  // add an outline to the tax lots which is only visible after zoom level 14.8
@@ -364,61 +401,116 @@ map.on('style.load', function() {
    }
  });
 
+///////////////////////////////////
+// this sets up the geojson as a source in the map, which I can use to add visual layers
+map.addSource('lenox-Hill', {
+  type: 'geojson',
+  data: './data/lenox-Hill.geojson',
+});
 
- // add an empty data source, which we will use to highlight the lot the user is hovering over
- map.addSource('highlight-feature', {
-   type: 'geojson',
-   data: {
-     type: 'FeatureCollection',
-     features: []
-   }
- })
+// add a custom-styled layer for tax lots
+map.addLayer({
+ id: 'lenox-Hill-lots-fill',
+ type: 'fill',
+ source: 'lenox-Hill',
+ paint: landUsePaint,
+}, 'waterway-label')
 
- // add a layer for the highlighted lot
- map.addLayer({
-   id: 'highlight-line',
-   type: 'line',
-   source: 'highlight-feature',
-   paint: {
-     'line-width': 3,
-     'line-opacity': 0.9,
-     'line-color': 'black',
+// add an outline to the tax lots which is only visible after zoom level 14.8
+map.addLayer({
+ id: 'lenox-Hill-lots-line',
+ type: 'line',
+ source: 'lenox-Hill',
+ paint: {
+   'line-opacity': 0.7,
+   'line-color': 'gray',
+   'line-opacity': {
+     stops: [[14, 0], [14.8, 1]], // zoom-dependent opacity, the lines will fade in between zoom level 14 and 14.8
    }
- });
+ }
+});
+////////////////////////
+// this sets up the geojson as a source in the map, which I can use to add visual layers
+map.addSource('upper-east-side', {
+  type: 'geojson',
+  data: './data/upper-east-side.geojson',
+});
+
+// add a custom-styled layer for tax lots
+map.addLayer({
+ id: 'upper-east-side-lots-fill',
+ type: 'fill',
+ source: 'upper-east-side',
+ paint: landUsePaint,
+}, 'waterway-label')
+
+// add an outline to the tax lots which is only visible after zoom level 14.8
+map.addLayer({
+ id: 'upper-east-side-lots-line',
+ type: 'line',
+ source: 'upper-east-side',
+ paint: {
+   'line-opacity': 0.7,
+   'line-color': 'gray',
+   'line-opacity': {
+     stops: [[14, 0], [14.8, 1]], // zoom-dependent opacity, the lines will fade in between zoom level 14 and 14.8
+   }
+ }
+});
+ // // add an empty data source, which we will use to highlight the lot the user is hovering over
+ // map.addSource('highlight-feature', {
+ //   type: 'geojson',
+ //   data: {
+ //     type: 'FeatureCollection',
+ //     features: []
+ //   }
+ // })
+ //
+ // // add a layer for the highlighted lot
+ // map.addLayer({
+ //   id: 'highlight-line',
+ //   type: 'line',
+ //   source: 'highlight-feature',
+ //   paint: {
+ //     'line-width': 3,
+ //     'line-opacity': 0.9,
+ //     'line-color': 'black',
+ //   }
+ // });
 
  // when the mouse moves, do stuff!
- map.on('mousemove', function (e) {
-   // query for the features under the mouse, but only in the lots layer
-   var features = map.queryRenderedFeatures(e.point, {
-       layers: ['east-harlem-lots-fill'],
-   });
-
-   // get the first feature from the array of returned features.
-   var lot = features[0]
-
-   if (lot) {  // if there's a lot under the mouse, do stuff
-     map.getCanvas().style.cursor = 'pointer';
- // make the cursor a pointer
-
-     // lookup the corresponding description for the land use code
-     var landuseDescription = LandUseLookup(parseInt(lot.properties.landuse)).description;
-
-     // use jquery to display the address and land use description to the sidebar
-     $('#address').text(lot.properties.address);
-     $('#bldgclass').text(lot.properties.bldgclass);
-     $('#landuse').text(landuseDescription);
-
-     // set this lot's polygon feature as the data for the highlight source
-     map.getSource('highlight-feature').setData(lot.geometry);
-   } else {
-     map.getCanvas().style.cursor = 'default'; // make the cursor default
-
-     // reset the highlight source to an empty featurecollection
-     map.getSource('highlight-feature').setData({
-       type: 'FeatureCollection',
-       features: []
-     });
-   }
- })
+ // map.on('mousemove', function (e) {
+ //   // query for the features under the mouse, but only in the lots layer
+ //   var features = map.queryRenderedFeatures(e.point, {
+ //       layers: ['east-harlem-lots-fill'],
+ //   });
+ //
+ //   // get the first feature from the array of returned features.
+ //   var lot = features[0]
+ //
+ //   if (lot) {  // if there's a lot under the mouse, do stuff
+ //     map.getCanvas().style.cursor = 'pointer';
+ // // make the cursor a pointer
+ //
+ //     // lookup the corresponding description for the land use code
+ //     var landuseDescription = LandUseLookup(parseInt(lot.properties.landuse)).description;
+ //
+ //     // use jquery to display the address and land use description to the sidebar
+ //     $('#address').text(lot.properties.address);
+ //     $('#bldgclass').text(lot.properties.bldgclass);
+ //     $('#landuse').text(landuseDescription);
+ //
+ //     // set this lot's polygon feature as the data for the highlight source
+ //     map.getSource('highlight-feature').setData(lot.geometry);
+ //   } else {
+ //     map.getCanvas().style.cursor = 'default'; // make the cursor default
+ //
+ //     // reset the highlight source to an empty featurecollection
+ //     map.getSource('highlight-feature').setData({
+ //       type: 'FeatureCollection',
+ //       features: []
+ //     });
+ //   }
+ // })
 
 })
